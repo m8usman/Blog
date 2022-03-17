0d670d5c1b352270f05a11b1d827be1b12477c66
Muhammad Usman <101389141+mu8man@users.noreply.github.com>
2022-03-17 18:01:41 +0500

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -123,8 +123,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'u8sman@gmail.com'
+EMAIL_HOST_PASSWORD = 'evjlwnditvenqusc'
 
 
 # Static files (CSS, JavaScript, Images)
@@ -132,6 +132,8 @@ EMAIL_HOST_PASSWORD = ''
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
