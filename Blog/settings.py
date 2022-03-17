bf35a0518227c40733b023dc22166a8d50d63ad0
Muhammad Usman <101389141+mu8man@users.noreply.github.com>
2022-03-17 10:23:12 +0500

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -123,8 +123,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'u8sman@gmail.com'
-EMAIL_HOST_PASSWORD = 'evjlwnditvenqusc'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 # Static files (CSS, JavaScript, Images)
